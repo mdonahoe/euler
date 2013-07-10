5c13170677633fb3a449273eb4f09abbb7ead57d
Matt Donahoe <matt.donahoe@gmail.com>
2013-07-10 04:26:43 -0700

    cleanup, and switch to native pow instead of my busted powmod

--- 429/e429.py
+++ 429/e429.py
@@ -1,92 +1,34 @@
 """
-#429
+429
 
-step 1: print the unitary divisors of n!
+Find the sum of the squares of "unitary" divisors of N!
+
+"Unitary divisors" are just relatively prime,
+so we can take the prime factorization of X and compute the powerset.
+The product of each subset is a unitary divisor.
+
+However, since we are just summing the squares,
+we dont need to compute the powerset, which is 2^N log(N)
+Instead we can use s = s * (1 + P_i^2),
+where P_i is a power of a prime which divides X.
+
+
+Also, since X is just N!, we can more easily compute the prime factorization,
+since we know all primes <= N are included
 """
 from prime_factorial import multiplicity
-
-def powerset(xs):
-    n = 2**(len(xs))
-    x = 0
-    while x < n:
-        i = x
-        a = []
-        j = 0
-        while i:
-            if i%2: a.append(xs[j])
-            j+=1
-            i/=2
-        yield a
-        x += 1
-
-def factorial_old(n):
-    return n > 1 and n * factorial(n-1) or 1
-
-def factors(n):
-    fs = [1,n]
-    for i in range(2, n):
-        if n % i == 0: fs.append(i)
-    return fs
-
-def lazy_gcd(a,b):
-    afs = set(factors(a))
-    bfs = set(factors(b))
-    return max(afs.intersection(bfs))
-
-def unitary_divisors(n):
-    fs = factors(n)
-    us = []
-    for f in fs:
-        g = n / f
-        if lazy_gcd(f, g) == 1: us.append(f)
-    return list(sorted(us))
-
-def product(xs, mod=None):
-    p = 1
-
-    if mod:
-        f = lambda p, x: ((p*x) % mod)
-    else:
-        f = lambda p, x: p*x
-
-    for x in xs:
-        p = f(p,x)
-
-    return p
-
-def old_S(n):
-    ds = [p**multiplicity(n, p) for p in primes(n)]
-    return sum(product(s)**2 for s in powerset(ds))
-
-
-def powmod(b, e, m):
-    if e == 0: return 1
-    p = b
-    while e > 1:
-        if e % 2:
-            p *= b
-        else:
-            p *= p
-        e = e >> 1
-        p = p % m
-    return p % m
 
 def S(n, m):
     s = 1
     for p in primes(n):
-        if p > n: break
         a = multiplicity(n, p)
-        x = powmod(p, a * 2, m)
+        x = pow(p, a * 2, m)
         s = (s * (x + 1)) % m
     return s
 
-def old_S_mod(n,m):
-    ps = [p for p in primes(n)]
-    ms = [multiplicity(n, p) for p in ps]
-    ds = [p**a for p,a in zip(ps, ms)]
-    return sum((product(s, m)**2) % m for s in powerset(ds)) % m
 
 def primes(n):
+    """yield primes from file"""
     with file('../primes.txt') as f:
         for line in f:
             try:
@@ -101,5 +43,5 @@ if __name__ == '__main__':
     M = 10**9 + 9
     N = 10**8
     print S(N, M)
-    # >>> 96997076
+    # >>> 98792821
 

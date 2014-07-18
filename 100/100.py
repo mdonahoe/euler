bc23d1c8206db9600ae219b003b10c0b2f3a7d41
Matt <matt@skyd.io>
2014-07-17 17:55:30 -0700

    formatting

--- 100/100.py
+++ 100/100.py
@@ -7,40 +7,45 @@ X3 = +BY2+C
 
 """
 from random import randint
-ns = [21, 120, 697, 4060]
-xs = [15, 85.0, 493.0, 2871.0]
-def fx(x,y,c):
-	return x*c[0]+y*c[1]+c[2]
-	
-def test(c,a):
-	score = 0
-	for i in range(len(ns)-1):
-		x = fx(xs[i],ns[i],c)
-		score += x - a[i+1]
-	return score
+
+def main():
+    ns = [21, 120, 697, 4060]
+    xs = [15, 85.0, 493.0, 2871.0]
+
+    def fx(x, y, c):
+        return x * c[0] + y* c[1] + c[2]
+
+    def test(c, a):
+        score = 0
+        for i in range(len(ns)-1):
+            x = fx(xs[i], ns[i], c)
+            score += x - a[i + 1]
+        return score
 
 
-genes = [(0,0,0)]
-def mutate(x):
-	if abs(sum(x))>10: x = (0,0,0)
-	return tuple([a+randint(-1,1) for a in x])
+    genes = [(0, 0, 0)]
+    def mutate(x):
+        if abs(sum(x))>10:
+            x = (0, 0, 0)
+        return tuple([a + randint(-1, 1) for a in x])
 
-def evolve(a):
-	genes = [(0,0,0)]
-	y=1
-	while genes and abs(y)>0:
-		x = genes.pop()
-		y = test(x,a)
-		#print x,y
-		genes.append(mutate(x))
-		genes.append(mutate(x))
-	return x
-		
-cx = evolve(xs)
-cn =  evolve(ns)
-n = 21
-x = 15
-while n<10**12:
-	x,n = fx(x,n,cx),fx(x,n,cn)
+    def evolve(a):
+        genes = [(0, 0, 0)]
+        y = 1
+        while genes and abs(y)>0:
+            x = genes.pop()
+            y = test(x, a)
+            genes.append(mutate(x))
+            genes.append(mutate(x))
+        return x
 
-print x,n
+    cx = evolve(xs)
+    cn =  evolve(ns)
+    n = 21
+    x = 15
+    while n < 10 ** 12:
+        x, n = fx(x, n, cx), fx(x, n, cn)
+
+    print x, n
+
+main()

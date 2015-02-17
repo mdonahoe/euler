ae7cd7ee590e6e950b11bfcb0a926b3d722b03d3
Matt <matt@skyd.io>
2015-02-16 18:19:31 -0800

    clean up coins question

--- 31/31.py
+++ 31/31.py
@@ -1,34 +1,19 @@
-#euler31
+# Project Euler 31
 
-"""
-##first attempt
-coins = [1,2,5,10,20,50,100,200]
-s=1
-for a in range(201):
- print a
- for b in range(101):
-  for c in range(41):
-   for d in range(21):
-    for e in range(11):
-     for f in range(5):
-      for g in range(3):
-       if a+2*b+5*c+10*d+20*e+50*f+100*g==200:
-        s+=1
- 
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-print s
+def possibles(amount_remaining, coin_index):
+    coin_value = coins[coin_index]
+    ways = 0
+    for num_coins in range(amount_remaining / coin_value + 1):
+        new_amount = amount_remaining - num_coins * coin_value
+        if new_amount == 0:
+            ways += 1
+            return ways
+        elif new_amount < 0:
+            return ways
+        elif coin_index > 0:
+            ways += possibles(new_amount, coin_index - 1)
+    return ways
 
-
-"""
-coins = [1,2,5,10,20,50,100,200]
-s = 0
-def possibles(base,coin):
-	global s
-	for i in range(200/coins[coin]+1):
-		x=base+coins[coin]*i
-		if x==200:s+=1;	return
-		elif x>200: return
-		elif coin>0: possibles(x,coin-1)
-		
-possibles(0,7)
-print s
+print possibles(200, len(coins) - 1)
